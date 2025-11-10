f4c2b3760dca894c58692b2a44474c1632f90029
Ed <60335944+Eddamame@users.noreply.github.com>
2025-11-10 23:14:51 +0800

    cleaned sort.c

--- sort.c
+++ sort.c
@@ -21,32 +21,16 @@ void swapRecords(StudentRecord *a, StudentRecord *b) {
     // *b = temp;
 }
 
+//refer to main.c and rmb to allow for asc and desc
 
 void sortById(StudentRecord records[], int count) {
     // (Bubble Sort for simplicity):
-    // Can try other sorting methods like merge sort etc
+    // Can try other sorting methods like merge sort quick sort etc
 
-    // IF count is less than or equal to 1, THEN RETURN (nothing to sort).
-
-    // Outer FOR loop from i = 0 up to (count - 2): // Controls passes
-        // Inner FOR loop from j = 0 up to (count - i - 2): // Controls comparisons
-            // IF records[j].id is GREATER THAN records[j + 1].id, THEN:
-                // Call swapRecords(&records[j], &records[j + 1]);
-
-    // Print success message ("CMS: Records sorted by ID.").
 }
 
 
 void sortByMark(StudentRecord records[], int count) {
     // (Bubble Sort, modified for descending order):
-    // Can try other sorting methods like merge sort etc
-
-    // IF count is less than or equal to 1, THEN RETURN (nothing to sort).
-
-    // Outer FOR loop from i = 0 up to (count - 2):
-        // Inner FOR loop from j = 0 up to (count - i - 2):
-            // IF records[j].mark is LESS THAN records[j + 1].mark, THEN: // Comparison flipped for DESCENDING
-                // Call swapRecords(&records[j], &records[j + 1]);
-
-    // Print success message ("CMS: Records sorted by Mark (High to Low).").
+    
 }
