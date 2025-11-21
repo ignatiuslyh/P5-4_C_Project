c0661ea2244a566ca62be36765dd50f75b82ad6e
project <123712365+AD22D-1@users.noreply.github.com>
2025-11-21 18:09:59 +0800

    update code

--- records.h
+++ records.h
@@ -7,11 +7,11 @@ extern "C" {
 
 #define STRING_LEN 50
 #define MAX_RECORDS 100
-#define ID_LEN 7
+
 
 // define struct 
 typedef struct StudentRecord {
-    int id[ID_LEN];
+    int id;
     char name[STRING_LEN];
     char programme[STRING_LEN];
     float mark;
